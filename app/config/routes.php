81b6dcf8e5dcb5c1760f07985ad72fec478702df
fkhq00183 <johnmanongsong05@gmail.com>
2026-09-03 12:36:55 +0800

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -51,3 +51,5 @@ $router->get('/student', 'StudentController::index');
 $router->get('/student/profile', 'StudentController::profile', [
     'middleware' => ['student']
 ]);
+
+$router->get('/users', 'UsersController::index');
